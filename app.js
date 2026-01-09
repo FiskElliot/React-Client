7ff24d747a8067676369d328716ca54b9fcce685
Elliot Jägstedt <elljag0707@edu.halmstad.se>
2026-01-09 14:30:11 +0100

    React lite

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 
+function Header(){
+    return (
+        <header>
+            <h1>My React App</h1>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </nav>
+        </header>
+    )
+}
+
 
 function App() {
   return (
     <div>
+        <Header></Header>
+
+
         <h2>React App</h2>
         <p>This is a p tag</p>
     </div>
